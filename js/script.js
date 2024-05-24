8bc08654e2f031403085c3df09a22a6e91762deb
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-05-24 17:44:17 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked() {
+
+  // input
+
+  let a = parseInt(document.getElementById('A').value)
+  let b = parseInt(document.getElementById('B').value)
+  let remainder = a
+  let quotient = 0
+
+  // process
+
+  while (true) {
+    if (remainder < b) {
+      break
+    } else {
+      quotient += 1
+      remainder -= b
+    }
+  }
+
+  // output
+
+  document.getElementById("user-info").innerHTML = quotient + " remainder " + remainder
+}
